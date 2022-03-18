cd26f0980a12b306bd9664f959e87b508a74f419
Enzo Sandré <atelier@enzosandre.fr>
2022-03-18 15:24:39 +0100

    Changement de méthode d'attente

--- CakeMachine/Fabrication/Opérations/AttenteIncompressible.cs
+++ CakeMachine/Fabrication/Opérations/AttenteIncompressible.cs
@@ -3,6 +3,6 @@
     internal static class AttenteIncompressible
     {
         public static void Attendre(TimeSpan howLong) => Thread.Sleep(howLong);
-        public static async Task AttendreAsync(TimeSpan howLong) => await Task.Delay(howLong);
+        public static async Task AttendreAsync(TimeSpan howLong) => await Task.Run(() => Thread.Sleep(howLong));
     }
 }
