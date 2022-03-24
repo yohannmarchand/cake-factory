e9d7b8c6df4f6863ba09b58b0c4b30b28941de45
Enzo Sandré <atelier@enzosandre.fr>
2022-03-24 09:04:54 +0100

    Ajout de types utiles aux algorithmes

---
+++ CakeMachine/Utils/Ring.cs
@@ -0,0 +1,22 @@
+﻿namespace CakeMachine.Utils
+{
+    internal class Ring<T>
+    {
+        private readonly Queue<T> _elements;
+
+        public Ring(IEnumerable<T> elements)
+        {
+            _elements = new Queue<T>(elements);
+        }
+
+        public T Next
+        {
+            get
+            {
+                var element = _elements.Dequeue();
+                _elements.Enqueue(element);
+                return element;
+            }
+        }
+    }
+}
